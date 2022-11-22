aabeb65fbf1542fd20b3cb9f8503d795e4cba740
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-22 19:05:10 +0100

    Add a logo to the header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,10 +1,13 @@
 import { Link } from "@remix-run/react";
+import logo from "../../public/img/logo.svg";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        <div className="logo"></div>
+        <Link to="/">
+          <img className="logo" src={logo} alt="app logo" />
+        </Link>
 
         <nav className="nav-header">
           <Link to="/">Home</Link>
