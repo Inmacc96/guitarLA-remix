f28bda04de010ed253283f347cd89d07dff5ddd1
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-22 17:36:43 +0100

    Add meta information in Remix

--- app/root.jsx
+++ app/root.jsx
@@ -1,3 +1,13 @@
+import { Meta } from "@remix-run/react";
+
+export function meta() {
+  return {
+    charset: "utf-8",
+    title: "GuitarLA - Remix",
+    viewport: "width=device-width, initial-scale-=1",
+  };
+}
+
 export default function App() {
   return (
     <Document>
@@ -10,7 +20,7 @@ function Document({ children }) {
   return (
     <html lang="es">
       <head>
-        <title>GuitarLA - Remix</title>
+        <Meta />
       </head>
       <body>{children}</body>
     </html>
