feb1b0068d4941698f7ebe0f02a9f062884ca943
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-22 18:56:41 +0100

    Adding content to the header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,5 +1,20 @@
+import { Link } from "@remix-run/react";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="logo"></div>
+
+        <nav className="nav-header">
+          <Link to="/">Home</Link>
+          <Link to="/aboutus">About Us</Link>
+          <Link to="/store">Store</Link>
+          <Link to="/blog">Blog</Link>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
