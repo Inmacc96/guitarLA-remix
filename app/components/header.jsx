84a339c015207ac3e41a47a78c69a38810c15de0
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-22 19:44:47 +0100

    Create nav component and used it in header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,9 +1,8 @@
-import { Link, useLocation } from "@remix-run/react";
+import { Link } from "@remix-run/react";
+import Nav from "./nav";
 import logo from "../../public/img/logo.svg";
 
 const Header = () => {
-  const location = useLocation();
-
   return (
     <header className="header">
       <div className="container container-header">
@@ -11,29 +10,7 @@ const Header = () => {
           <img className="logo" src={logo} alt="app logo" />
         </Link>
 
-        <nav className="nav-header">
-          <Link to="/" className={location.pathname === "/" ? "active" : ""}>
-            Home
-          </Link>
-          <Link
-            to="/aboutus"
-            className={location.pathname === "/aboutus" ? "active" : ""}
-          >
-            About Us
-          </Link>
-          <Link
-            to="/store"
-            className={location.pathname === "/store" ? "active" : ""}
-          >
-            Store
-          </Link>
-          <Link
-            to="/blog"
-            className={location.pathname === "/blog" ? "active" : ""}
-          >
-            Blog
-          </Link>
-        </nav>
+        <Nav />
       </div>
     </header>
   );
---
+++ app/components/nav.jsx
@@ -0,0 +1,33 @@
+import { Link, useLocation } from "@remix-run/react";
+
+const Nav = () => {
+  const location = useLocation();
+
+  return (
+    <nav className="nav-header">
+      <Link to="/" className={location.pathname === "/" ? "active" : ""}>
+        Home
+      </Link>
+      <Link
+        to="/aboutus"
+        className={location.pathname === "/aboutus" ? "active" : ""}
+      >
+        About Us
+      </Link>
+      <Link
+        to="/store"
+        className={location.pathname === "/store" ? "active" : ""}
+      >
+        Store
+      </Link>
+      <Link
+        to="/blog"
+        className={location.pathname === "/blog" ? "active" : ""}
+      >
+        Blog
+      </Link>
+    </nav>
+  );
+};
+
+export default Nav;
