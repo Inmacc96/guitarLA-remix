f91beb50a34249b17fd76c6932ebb480af4f29bd
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-23 17:17:24 +0100

    Design the guitar component

--- app/components/guitar.jsx
+++ app/components/guitar.jsx
@@ -1,5 +1,25 @@
+import { Link } from "@remix-run/react";
+
 const Guitar = ({ guitar }) => {
-  return <div>{guitar.name}</div>;
+  const { name, description, image, price, url } = guitar;
+
+  return (
+    <div className="guitar">
+      <img
+        src={image.data.attributes.formats.medium.url}
+        alt={`${name} guitar`}
+      />
+      <div className="content">
+        <h3>{name}</h3>
+        <p className="description">{description}</p>
+        <p className="price">${price}</p>
+
+        <Link className="link" to={`/guitars/${url}`}>
+          View product
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Guitar;
