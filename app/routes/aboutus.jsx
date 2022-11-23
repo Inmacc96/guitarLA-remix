5e57d3867357ea7c4e386d24da6a205b09501343
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-23 15:54:06 +0100

    Design the about us page

--- app/routes/aboutus.jsx
+++ app/routes/aboutus.jsx
@@ -1,8 +1,35 @@
+import image from "../../public/img/aboutus.jpg";
 
 const Aboutus = () => {
   return (
-    <div>Aboutus</div>
-  )
-}
+    <main className="container">
+      <h2 className="heading">About Us</h2>
 
-export default Aboutus
+      <div className="content">
+        <img src={image} alt="about us" />
+
+        <div>
+          <p>
+            Suspendisse vel nulla non turpis scelerisque fringilla. In vel
+            tortor vitae sem aliquam mattis. Donec a metus sodales, elementum
+            erat ut, posuere ipsum. Donec eget condimentum mi, vitae pretium
+            purus. Integer fermentum sem metus, sit amet laoreet nisl fringilla
+            non. Pellentesque eu arcu volutpat, imperdiet neque sit amet,
+            lacinia urna.
+          </p>
+
+          <p>
+            Suspendisse vel nulla non turpis scelerisque fringilla. In vel
+            tortor vitae sem aliquam mattis. Donec a metus sodales, elementum
+            erat ut, posuere ipsum. Donec eget condimentum mi, vitae pretium
+            purus. Integer fermentum sem metus, sit amet laoreet nisl fringilla
+            non. Pellentesque eu arcu volutpat, imperdiet neque sit amet,
+            lacinia urna.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Aboutus;
