872f39fceb5e3c0abbc74d67e941ba6deb85e08c
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-23 16:47:56 +0100

    Get the guitars from the strapi API on the server-side

---
+++ app/models/guitars.server.js
@@ -0,0 +1,5 @@
+export async function getGuitars() {
+  const response = await fetch(`${process.env.API_URL}/guitars?populate=image`);
+  const result = await response.json();
+  return result;
+}
--- app/routes/store.jsx
+++ app/routes/store.jsx
@@ -1,3 +1,10 @@
+import { getGuitars } from "~/models/guitars.server";
+
+export async function loader() {
+  const guitars = await getGuitars();
+  return guitars;
+}
+
 const Store = () => {
   return <div>Store</div>;
 };
