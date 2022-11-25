f2ce9490a290ffb9f3e8fab58c0a829c1dfc948c
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-25 10:44:10 +0100

    Format the post publication date

--- app/components/post.jsx
+++ app/components/post.jsx
@@ -1,4 +1,5 @@
 import { Link } from "@remix-run/react";
+import { formatDate } from "~/utils/helpers";
 
 const Post = ({ post }) => {
   const { title, content, image, url, publishedAt } = post;
@@ -11,7 +12,7 @@ const Post = ({ post }) => {
       />
       <div className="content">
         <h3>{title}</h3>
-        <p className="date">{publishedAt}</p>
+        <p className="date">{formatDate(publishedAt)}</p>
         <p className="summary">{content}</p>
         <Link className="link" to={`/posts/${url}`}>
           Read Post
