f20613c381ddbb11869c3c33e9dbef10b39f19f0
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-28 17:23:07 +0100

    Solving the hydration problem in Remix

--- app/routes/cart.jsx
+++ app/routes/cart.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useOutletContext } from "@remix-run/react";
+import { ClientOnly } from "remix-utils";
 import styles from "~/styles/cart.css";
 
 export function meta() {
@@ -31,69 +32,76 @@ const Cart = () => {
   }, [cart]);
 
   return (
-    <main className="container">
-      <h1 className="heading">Shopping Cart</h1>
+    <ClientOnly fallback={"Loading..."}>
+      {() => (
+        <main className="container">
+          <h1 className="heading">Shopping Cart</h1>
 
-      <div className="content">
-        <div className="cart">
-          <h2>Items</h2>
+          <div className="content">
+            <div className="cart">
+              <h2>Items</h2>
 
-          {cart.length === 0
-            ? "Empty cart"
-            : cart.map((product) => (
-                <div key={product.id} className="product">
-                  <div>
-                    <img src={product.image} alt={`product ${product.name}`} />
-                  </div>
+              {cart?.length === 0
+                ? "Empty cart"
+                : cart?.map((product) => (
+                    <div key={product.id} className="product">
+                      <div>
+                        <img
+                          src={product.image}
+                          alt={`product ${product.name}`}
+                        />
+                      </div>
 
-                  <div>
-                    <p className="name">{product.name}</p>
-                    <p className="quantity">Quantity:</p>
+                      <div>
+                        <p className="name">{product.name}</p>
+                        <p className="quantity">Quantity:</p>
 
-                    <select
-                      value={product.quantity}
-                      className="select-quantity"
-                      onChange={(e) =>
-                        updateQuantity({
-                          id: product.id,
-                          quantity: +e.target.value,
-                        })
-                      }
-                    >
-                      <option value="0">-- Select --</option>
-                      <option value="1">1</option>
-                      <option value="2">2</option>
-                      <option value="3">3</option>
-                      <option value="4">4</option>
-                      <option value="5">5</option>
-                    </select>
+                        <select
+                          value={product.quantity}
+                          className="select-quantity"
+                          onChange={(e) =>
+                            updateQuantity({
+                              id: product.id,
+                              quantity: +e.target.value,
+                            })
+                          }
+                        >
+                          <option value="0">-- Select --</option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                        </select>
 
-                    <p className="price">
-                      $ <span>{product.price}</span>
-                    </p>
-                    <p className="subtotal">
-                      Subtotal: ${" "}
-                      <span>{product.price * product.quantity}</span>
-                    </p>
-                  </div>
+                        <p className="price">
+                          $ <span>{product.price}</span>
+                        </p>
+                        <p className="subtotal">
+                          Subtotal: ${" "}
+                          <span>{product.price * product.quantity}</span>
+                        </p>
+                      </div>
 
-                  <button
-                    type="button"
-                    className="btn-delete"
-                    onClick={() => deleteGuitar(product.id)}
-                  >
-                    X
-                  </button>
-                </div>
-              ))}
-        </div>
+                      <button
+                        type="button"
+                        className="btn-delete"
+                        onClick={() => deleteGuitar(product.id)}
+                      >
+                        X
+                      </button>
+                    </div>
+                  ))}
+            </div>
 
-        <aside className="summary">
-          <h3>Order summary</h3>
-          <p>Total to be paid: $ {total}</p>
-        </aside>
-      </div>
-    </main>
+            <aside className="summary">
+              <h3>Order summary</h3>
+              <p>Total to be paid: $ {total}</p>
+            </aside>
+          </div>
+        </main>
+      )}
+    </ClientOnly>
   );
 };
 
