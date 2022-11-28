aad0749729900dc2fd242057e12e2fd65934f7c0
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-28 11:10:31 +0100

    Define and design of the cart page

--- app/routes/cart.jsx
+++ app/routes/cart.jsx
@@ -1,8 +1,38 @@
+import styles from "~/styles/cart.css";
+
+export function meta() {
+  return {
+    title: "GuitarLA - Shopping Cart",
+    description: "Guitars for sale, music, blog, shopping cart, shop",
+  };
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
 
 const Cart = () => {
   return (
-    <div>Cart</div>
-  )
-}
+    <main className="container">
+      <h1 className="heading">Shopping Cart</h1>
 
-export default Cart
+      <div className="content">
+        <div className="cart">
+          <h2>Items</h2>
+        </div>
+
+        <aside className="summary">
+          <h3>Order summary</h3>
+          <p>Total to be paid: $</p>
+        </aside>
+      </div>
+    </main>
+  );
+};
+
+export default Cart;
